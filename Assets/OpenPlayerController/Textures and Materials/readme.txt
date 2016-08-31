3e79bd91f271b0f2e29f1e02146035df13d826f7
Elmar Talibzade <elmar1235@gmail.com>
2016-08-31 19:02:03 +0400

    Repository Directory Re-Structure
    "Project Files" now contain Unity scripts. "Builds" Folder contains
    compiled .unitypackage files.

--- Assets/OpenPlayerController/Textures and Materials/readme.txt
+++
@@ -1,5 +0,0 @@
-This pack contains textures and materials for prototyping.
-
-Prototype textures are very helpful to measure the scale of textures, to see the distribution of textures during uv-mapping and to visualize an idea of an early level design of a prototype game.
-
-(C) Dexsoft
---
+++ Project Files/Assets/OpenPlayerController/Scripts/FPS Controller/FPS_Jump.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class FPS_Jump : MonoBehaviour {
+
+    public float jumpForce = 2.5f;
+
+    public bool canJump = true;
+    public bool isGrounded = true;
+
+    public float AirLocomotionSpeedDegradation = 0.2f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, GetComponent<Collider>().bounds.extents.y + 0.01f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canJump)
+            {
+                if (isGrounded)
+                {
+                    if (GetComponent<FPS_Locomotion>().crouchEnabled)
+                    {
+                        if (!GetComponent<FPS_Crouch>().isCrouching)
+                        {
+                            Jump();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
