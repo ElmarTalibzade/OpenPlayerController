6ce41f5b3195decb30506e75645b9cec69bd9e2e
HardCode Lab <accounts@hardcodelab.com>
2017-10-23 10:14:34 +0100

    Added a script that handles all input of OpenPlayerController

---
+++ Project Files/Assets/OpenPlayerController/Scripts/FPS Controller/FPS_InputController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This component handles all user input, controlling other aspects of FPS Player such as jumping and climbing
+/// </summary>
+public class FPS_InputController : MonoBehaviour
+{
+    private FPS_CameraLook component_CameraLook;
+    private FPS_Climb component_Climb;
+    private FPS_Jump component_Jump;
+    private FPS_Locomotion component_Locomotion;
+    private FPS_Crouch component_Crouch;
+    
+    void Start()
+    {
+        component_CameraLook = transform.GetChild(0).gameObject.GetComponent<FPS_CameraLook>();
+        component_Climb = GetComponent<FPS_Climb>();
+        component_Jump = GetComponent<FPS_Jump>();
+        component_Locomotion = GetComponent<FPS_Locomotion>();
+        component_Crouch = GetComponent<FPS_Crouch>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
