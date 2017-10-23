93548b6148b201f7475464f4fb15e3042f0a02e1
HardCode Lab <accounts@hardcodelab.com>
2017-10-23 10:18:44 +0100

    FPS_CameraLook script now receives input from FPS_InputController

--- Project Files/Assets/OpenPlayerController/Scripts/FPS Controller/FPS_CameraLook.cs
+++ Project Files/Assets/OpenPlayerController/Scripts/FPS Controller/FPS_CameraLook.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(FPS_InputController))]
 public class FPS_CameraLook : MonoBehaviour
 {
     public float lookSensitivity = 5;                                       //how sensitive camera's rotation is to user's mouse input; setting it 0 would render camera static
@@ -11,33 +12,32 @@ public class FPS_CameraLook : MonoBehaviour
     public float maxXRotation = 90;
 
     //used to store mouse movement input * lookSensitivity
-    private float XRotation;
+    private float YRot;
 
-    private float YRotation;
+    private float XRot;
 
     private float XRotationV;
     private float YRotationV;
 
     //final camera rotations are stored by these variables
     public float currentXRotation;
-
     public float currentYRotation;
 
-    private void Update()
+    public void Look(Vector2 DIR)
     {
         //store mouse input based on X and Y axis times the look sensitivity
-        XRotation += -Input.GetAxis("Mouse Y") * lookSensitivity;
-        YRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        YRot += -DIR.y * lookSensitivity;
+        XRot += DIR.x * lookSensitivity;
 
         //clamp the XRotation of the camera look based on two variables
-        XRotation = Mathf.Clamp(XRotation, minXRotation, maxXRotation);
+        YRot = Mathf.Clamp(YRot, minXRotation, maxXRotation);
 
         //this is where camera's X and Y rotation values are stored based on smooth damp, which would basically make camera rotate smoothly to the target value for the current frame (XRotation and YRotation found before)
-        currentXRotation = Mathf.SmoothDamp(currentXRotation, XRotation, ref XRotationV, lookSmoothDamp);
-        currentYRotation = Mathf.SmoothDamp(currentYRotation, YRotation, ref YRotationV, lookSmoothDamp);
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, YRot, ref XRotationV, lookSmoothDamp);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, XRot, ref YRotationV, lookSmoothDamp);
     }
 
-    private void LateUpdate()
+    void LateUpdate()
     {
         //with all calculations done, set camera's X and Y rotation based on values we found earlier; ensure that Z axis stays 0 every frame
         transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
